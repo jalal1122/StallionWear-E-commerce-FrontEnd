8dab276fc206978d550283f0e113b284b1157972
Anis Riaz <masterkha123789@gmail.com>
2025-08-11 01:14:56 +0500

    done list

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -2,11 +2,13 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer";
+import { FaTrash, FaShoppingCart } from "react-icons/fa";
 import {
   fetchWishlist,
   removeWishlistItem,
 } from "../features/Wishlist/wishlistSlice";
 import { addToCart } from "../features/Cart/cartSlice";
+// import { useNavigate } from "react-router";
 
 const Wishlist = () => {
   const dispatch = useDispatch();
@@ -26,12 +28,10 @@ const Wishlist = () => {
   };
 
   const handleRemoveFromWishlist = (itemId) => {
-    if (token) {
-      dispatch(removeWishlistItem({ itemId, token }));
-    } else {
-      console.log("User is not authenticated");
-    }
+      dispatch(removeWishlistItem({ itemId}));
   };
+
+
 
   if (loading) {
     return <div className="text-center mt-10">Loading...</div>;
@@ -69,25 +69,28 @@ const Wishlist = () => {
             </a>
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
+          <div className="flex flex-wrap items-center justify-center gap-10 mt-10">
             {wishlistProducts.map((item) => (
               <div
                 key={item?.product?._id}
                 className="border p-4 rounded-lg shadow-md"
               >
+                <img src={item?.product?.images[0]} alt={item?.product?.name} className="w-[200px] h-auto object-cover rounded-md" />
+
                 <h2 className="text-xl font-semibold">{item?.product?.name}</h2>
                 <p className="text-gray-600">${item?.priceAtTime}</p>
                 <button
                   onClick={() => handleAddToCart(item)}
-                  className="mt-4 w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                  className="flex items-center justify-center mt-3 w-full bg-black text-white rounded-full px-4 py-2 rounded hover:bg-gray-600"
                 >
-                  Add to Cart
+                  <FaShoppingCart className="mx-2" />
+                  Add to Cart 
                 </button>
                 <button
                   onClick={() => handleRemoveFromWishlist(item._id || item.id)}
-                  className="mt-2 w-full bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                  className="flex items-center justify-center mt-2 w-full bg-black text-white px-4 py-2 rounded-full hover:bg-red-600"
                 >
-                  Remove from Wishlist
+                  <FaTrash />
                 </button>
               </div>
             ))}
