40af9502b83855a55e748ef6ecec4e0a15240f20
Anis Riaz <masterkha123789@gmail.com>
2025-08-19 00:43:03 +0500

    done

--- src/Components/LandingPage/BrowseByCategories.jsx
+++ src/Components/LandingPage/BrowseByCategories.jsx
@@ -6,7 +6,7 @@ import Shoes from "../../Images/Shoes.png";
 import { useSelector } from "react-redux";
 
 const Categories = () => {
-  const { secondaryBg } = useSelector((state) => state.colors.colors);
+  const { primaryBg, primaryText } = useSelector((state) => state.colors.colors);
 
   const categoriesList = [
     "Jackets",
@@ -22,9 +22,9 @@ const Categories = () => {
     <>
       <div
         className="categories-container py-16 px-5 sm:px-20"
-        style={{ backgroundColor: secondaryBg }}
+        style={{ backgroundColor: primaryBg, color: primaryText }}
       >
-        <h2 className="sm:text-5xl text-4xl text-black font-extrabold text-center mb-12 tracking-wide">
+        <h2 className="sm:text-5xl text-4xl font-extrabold text-center mb-12 tracking-wide">
           Browse By Categories
         </h2>
         <div className="category-layer-1 flex flex-wrap justify-center gap-10">
@@ -34,21 +34,22 @@ const Categories = () => {
                 key={index}
                 className={`category-${
                   index + 1
-                } w-[400px] h-[250px] flex flex-row items-center justify-around text-center px-4 py-2 gap-2 rounded-lg shadow-2xl hover:transition-shadow duration-300 cursor-pointer overflow-hidden`}
-                style={{
-                  backgroundColor: "white",
-                }}
+                } w-[200px] h-[250px] flex flex-col items-center justify-center mt-5 text-center px-8 py-2 gap-2 rounded-lg shadow-2xl hover:transition-shadow duration-300 cursor-pointer overflow-hidden`}
+               style={{
+                  backgroundColor: primaryText === "#fff" ? "#1F2937" : "",
+
+                  }}
               >
                 <h1
-                  className="sm:text-3xl text-xl text-black font-bold"
-                  style={{ color: "black" }}
+                  className="sm:text-xl text-lg font-bold"
+                  style={{ color: primaryText }}
                 >
                   {category}
                 </h1>
                 <img
                   src={categoryImages[index]}
                   alt={category}
-                  className="rounded-md aspect-[4/3] w-50 h-[200px] object-cover object-top"
+                  className="rounded-md aspect-[4/3] w-40 h-35 object-cover object-top"
                 />
               </div>
             );
